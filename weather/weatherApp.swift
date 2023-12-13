dd3b97942cd42ad612b11eb622ecea32dea15859
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-13 14:30:32 +0530

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Diptayan Jash on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
