c6bcce49a63c74feb1679f45c24b3e506e4013c2
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-19 17:38:49 +0530

    Add SafariView.swift for displaying web content

---
+++ weather/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariView.swift
+//  weather
+//
+//  Created by Diptayan Jash on 19/12/23.
+//
+
+import Foundation
+import SafariServices
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
